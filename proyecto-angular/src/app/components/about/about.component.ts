109e5329305148a828f68d85e2a79c2595870ffb
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-08-15 12:51:59 +0300

    styles article

--- proyecto-angular/src/app/components/about/about.component.ts
+++ proyecto-angular/src/app/components/about/about.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  
+  public title: string;
+  public subtitle: string;
+  public web: string;
 
-  constructor() { }
+  constructor() { 
+    this.title = 'juan camilo';
+    this.subtitle = 'desarrollador web';
+    this.web = 'web.com';
+  }
 
   ngOnInit(): void {
   }
