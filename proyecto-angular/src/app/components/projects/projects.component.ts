a10634dfcc1450346c9bc040459f3bdfce845675
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-08-17 10:18:08 +0300

    get list of projects of bakend and show on frontend

--- proyecto-angular/src/app/components/projects/projects.component.ts
+++ proyecto-angular/src/app/components/projects/projects.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project';
+import { ProjectService } from '../../services/project.service';
+import { Global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  public projects: Project[] = [];
+
+  constructor(
+    private _projectService: ProjectService
+  ) { 
+    
+  }
 
   ngOnInit(): void {
+    this.getProjects();
+  }
+  getProjects(){
+  	this._projectService.getProjects().subscribe(
+  		response => {
+        
+  			if(response.projects){
+  				this.projects = response.projects;
+  			}
+       console.log(response)
+  		},
+  		error => {
+  			console.log(<any>error);
+  		}
+  	);
   }
 
 }
